cf3c8113b0486f125a8d7153a945122f4464ec97
eugenevanifatev <eugenevanifatev@gmail.com>
2021-11-19 20:06:46 +0300

    add Unit4Case4

---
+++ Unit4/Case4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit4
+{
+//    Задание 4.4: 
+//1. Придумать свой собственный класс, чтобы он имел 3 поля разных типов и 2 метода
+//2. Создать два экземпляра класса, инициализировать их поля и вызвать методы
+
+    internal class Case4
+    {
+        public int number;
+        public bool action;
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Metod \"DoMultiplication\" return number * 2 if action is true.");
+        }
+
+        public int DoMultiplication(int number, bool action)
+        {
+            if (action)
+            {
+                return number * 2;
+            }
+            else
+            {
+                return number;
+            }
+        }
+    }
+}
--- Unit4/Program.cs
+++ Unit4/Program.cs
@@ -8,6 +8,14 @@ namespace Unit4
         {
             Case2 case2 = new Case2();
             case2.GetCalculator();
+
+            Case4 case4_1 = new Case4(); 
+            case4_1.GetInfo();
+            Console.WriteLine(case4_1.DoMultiplication(5, true));
+
+            Case4 case4_2 = new Case4();
+            case4_2.GetInfo();
+            Console.WriteLine(case4_2.DoMultiplication(5, false));
         }
     }
 }
